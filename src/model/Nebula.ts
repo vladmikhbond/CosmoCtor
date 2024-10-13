9a7540df4296cc5323252fe02c57f6e72ca5d35b
vladmikhbond <vladmikhbond@gmail.com>
2024-10-13 16:38:08 +0300

    nebula - ok

--- src/model/Nebula.ts
+++ src/model/Nebula.ts
@@ -5,31 +5,40 @@ import Space from "./Space.js";
 export default class Nebula
 {
  
-    static duoIntegral(M:number, R:number):number[] 
+    static calcAccelerations(M:number, R:number):number[] 
     {
         let arr = new Array(R|0 + 1).fill(0);
         const N = 200;
         for (let a = 1; a <= R; a += 1) {
-            arr[a] = duoIntegral1(M, R, a, N);
+            arr[a] = doubleIntegral(M, R, a, N);
         }
         return arr;
 
         // --------------- local function -------------------
 
-        function duoIntegral1(M:number, R:number, a:number, n:number):number {
-            const dw = Math.PI / n, dz = R / n,  dm = M / (n**2);
-            let fx = 0;
-            for (let w = 0; w < Math.PI; w += dw) {
-                for (let z = 0; z < R; z += dz) {
-                    let r = Math.sqrt(z * z + a * a - 2 * a * z * Math.cos(w));
+        function doubleIntegral(M:number, R:number, a:number, n:number):number 
+        {
+            const dw = Math.PI / n, 
+                  dz = R / n,  
+                  dm = M / (n**2),
+                  aa = a * a;
+            let force = 0;
+
+            for (let w = 0; w < Math.PI; w += dw) 
+            {
+                let cosW = Math.cos(w + dw/2); 
+                let a2cosW = 2 * a * cosW; 
+
+                for (let z = 1; z < R; z += dz) {
+                    let r = Math.sqrt(z * z + aa - z * a2cosW);
                     if (r) {
-                        let rx = a - z * Math.cos(w + dw/2);
-                        fx += rx/(r**3) * dm;
+                        let rx = a - z * cosW;
+                        force += dm * rx / r**3;
                     }
                 } 
             } 
-            fx *= 2 * M / R;
-            return fx;
+            // NOTE: negative values are forbidden (they are possible when n is too small)
+            return force > 0 ? force * 2 * M / R : 0;
         }
 
     }
@@ -73,11 +82,13 @@ export default class Nebula
         let pieces = Nebula.splitOnPieces(n, nebulaR, proto);
 
         // set init velocities
-        let axels = Nebula.duoIntegral(proto.m, nebulaR);
-        for (let piece of pieces) {
-            
+        let accels = Nebula.calcAccelerations(proto.m, nebulaR);
+
+        // console.log(accels)    ///////////////////////
+
+        for (let piece of pieces) {            
             let r = Math.sqrt(piece.x**2 + piece.y**2);
-            let v = Math.sqrt(axels[r|0] * r);
+            let v = Math.sqrt(accels[r|0] * r);
             let angle = Math.atan2(piece.y, piece.x) + Math.PI / 2;
 
             let dir = Math.random() > veloK ? 1 : -1;                           
@@ -85,21 +96,7 @@ export default class Nebula
             piece.vy = dir * v * (Math.sin(angle)); 
         }
 
-
-        // for (let piece of pieces) {
-            
-        //     let r = Math.sqrt(piece.x**2 + piece.y**2);
-
-        //     // velo direction
-        //     let dir = Math.random() > veloK ? 1 : -1;   
-        //     let k = 2; 
-        //     let v = k * Math.sqrt(proto.m * r / nebulaR**2);
-        //     let angle = Math.atan2(piece.y, piece.x) + Math.PI / 2; 
-        //     piece.vx = dir * v * (Math.cos(angle)); 
-        //     piece.vy = dir * v * (Math.sin(angle)); 
-        // }
-
-        // center of mass
+        // find center of mass
         let msum = 0, mx = 0, my = 0, mvx = 0, mvy = 0; 
         for (let p of pieces) {
             mx += p.x * p.m;
@@ -113,7 +110,8 @@ export default class Nebula
         my /= msum;
         mvx /= msum;
         mvy /= msum;
-         
+        
+        // final places & velocities of pieces
         for (let piece of pieces) {
             piece.x += proto.x - mx;  
             piece.y += proto.y - my;
@@ -121,9 +119,7 @@ export default class Nebula
             piece.vy += proto.vy - mvy;
             space.planets.push(piece);
         }
-
-
-        // 
+ 
         space.removePlanet(proto);
 
     }
