6aa1d242d1fb462d55bfe7e85faa991d279fe217
vladmikhbond <vladmikhbond@gmail.com>
2025-05-26 20:43:47 +0300

    .

--- ts/controller/TaskController.ts
+++ ts/controller/TaskController.ts
@@ -19,12 +19,8 @@ export default class TaskController
     selectedTask: TaskData | null = null; 
 
     constructor(public controller: Controller) {
-        this.bindTaskDivMouseEvents();
-        this.bindTaskEvents();
-
 
         doc.loadSceneButton.addEventListener('click', () => {            
-            this.taskButtonsMaker();
             this.loadScene(doc.savedSceneArea.value);
         });
 
@@ -41,7 +37,6 @@ export default class TaskController
             let idx = +doc.sceneSelect.value;
             if (idx == 0) {
                 this.controller.clearScene();
-                doc.taskDiv.style.display = 'none';
                 return;
             }    
             let problem = this.problems[idx];
@@ -53,15 +48,7 @@ export default class TaskController
             doc.problemBoard.style.display = 'block'; 
             doc.problemBoard.style.backgroundColor = 'rgba(241, 241, 10, 0.1)';
             doc.answerText.style.display = problem.isAnswerNumber ? 'inline' : 'none';
-            
 
-
-
-            // doc.openHelpButton.style.display = doc.openSolvButton.style.display = 'block';
-            // doc.taskDiv.style.display = 'block';
-            // doc.helpDiv.style.display = 'none';
-            // doc.solvDiv.style.display = 'none';
-            // (<HTMLDivElement>doc.condDiv.firstElementChild).innerHTML = problem.cond;
         }
 
         doc.sceneSelect.addEventListener("change", loadProblemInitScene);
@@ -96,103 +83,10 @@ export default class TaskController
         }
     }
 
-
-
-    // to grag task panels 
-    //
-    private bindTaskDivMouseEvents() {
-
-        let cursor: {x: number, y: number} | null = null;
-   
-        doc.taskDiv.addEventListener('mousedown', (e: MouseEvent) => {
-            cursor = {x: e.screenX, y: e.screenY};
-        });
-
-        doc.taskDiv.addEventListener('mousemove', (e: MouseEvent) => { 
-            if (cursor) {                              
-                let dx = e.screenX - cursor.x;
-                let dy = e.screenY - cursor.y;
-                let style = window.getComputedStyle(doc.taskDiv);
-
-                let left = parseFloat(style.left) + dx;
-                doc.taskDiv.style.left = left + 'px';
-
-                let top = parseFloat(style.top) + dy;
-                doc.taskDiv.style.top = top + 'px';
-
-                cursor = {x: e.screenX, y: e.screenY};
-            }    
-        });
-
-        doc.taskDiv.addEventListener('mouseup', (e: MouseEvent) => {
-            cursor = null;
-        });
-
-    }
-
-    private bindTaskEvents()  
-    {
-        doc.openHelpButton.addEventListener('click', () => {
-            doc.helpDiv.style.display='block'; 
-            doc.openHelpButton.style.display='none';
-        });
-
-        doc.openSolvButton.addEventListener('click', () => {
-            doc.solvDiv.style.display='block'; 
-            doc.openSolvButton.style.display='none';
-            this.loadScene(this.selectedTask!.final);          
-        });
-
-        doc.closeTaskButton.addEventListener('click', () => {
-            doc.taskDiv.style.display = 'none';
-        });
-
-    }
-
-    // Створює кнопки завдання і саджає не кожну свій обробник.
-    //
-    taskButtonsMaker() {
-        const value = doc.dataArea.value;
-        const loaderFunction = new Function("", `return [${value}];`);
-        const data: TaskData[] = loaderFunction();
-        
-        doc.menuSpan.innerHTML = '';
-        for (let task of data) {
-            let taskButton = document.createElement('Button');
-            taskButton.style.backgroundImage = `url('${task.imurl}')`;
-            // taskButton.style.backgroundImage = `url('./assets/${task.id}.png')`;
-            taskButton.title = task.title;
-            
-            // Обробник натискання на кнопку завдання
-            taskButton.addEventListener('click', () => {
-                this.selectedTask = task;
-                this.loadScene(task.init);
-
-                doc.openHelpButton.style.display = doc.openSolvButton.style.display = 'block';
-                doc.taskDiv.style.display = 'block';
-                doc.helpDiv.style.display = 'none';
-                doc.solvDiv.style.display = 'none';
-
-                // (<HTMLDivElement>doc.condDiv.firstElementChild).innerHTML = task.cond;
-                (<HTMLDivElement>doc.helpDiv.firstElementChild).innerHTML = task.help;
-                (<HTMLDivElement>doc.solvDiv.firstElementChild).innerHTML = task.solv;
-
-                // Математичні формули у динамічному контенті
-                (new Function("","MathJax.typeset()"))();            
-            })
-            doc.menuSpan.append(taskButton); 
-
-        }
-
-    }
-
-
     loadScene(data: string) {
         const space = this.controller.space;
         const view = this.controller.view;
 
-
-        
         space.planets = [];
         space.starters = [];
         if (data) {
